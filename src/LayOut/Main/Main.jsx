fa564022297c617126088ad9a8e60445871500e6
irfanurislam <mdirfanurislam@gmail.com>
2023-08-09 15:31:36 +0600

    first

--- src/LayOut/Main/Main.jsx
+++ src/LayOut/Main/Main.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
+            <h1>this is our main jsx</h1>
             <Outlet></Outlet>   
         </div>
     );
