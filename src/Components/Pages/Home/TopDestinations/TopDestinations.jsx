4f997462cd567d1baf00c5fe6d2f40bd454900d6
Habibullah Raju <raju.ra423@gmail.com>
2023-08-09 22:13:54 +0600

    i added Top Things to Do by category section from habibullah

--- src/Components/Pages/Home/TopDestinations/TopDestinations.jsx
+++ src/Components/Pages/Home/TopDestinations/TopDestinations.jsx
@@ -7,7 +7,12 @@ import { Navigation } from 'swiper/modules';
 
 const TopDestinations = () => {
     return (
-        <div>
+        <div className='bg-white'>
+          <div>
+            <h1 className='text-xl font-bold'>Top Things to Do by category</h1>
+            <h4 className='my-1 mb-4'>Travelers' Choice Best of the Best winners</h4>
+          </div>
+           <div>
            <Swiper
       spaceBetween={50}
       slidesPerView={4}
@@ -16,13 +21,45 @@ const TopDestinations = () => {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List for Habibullah</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+        <img className='h-64  relative object-cover' src="https://images.unsplash.com/photo-1687360440731-d124aea41a3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <h2 className='absolute bottom-6 font-extrabold text-3xl text-white left-6 shadow-2xl'>Bucket List</h2>
+        </div>
+      </SwiperSlide>
+     
     </Swiper>
+           </div>
         </div>
     );
 };
