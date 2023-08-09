277fcdacc290c451be444876b8760c24c9a8c1af
riyad3399 <programminghero.riyad@gmail.com>
2023-08-09 23:41:53 +0600

    error from riyad

--- src/Components/Pages/Home/Feedback/Feedback.jsx
+++ src/Components/Pages/Home/Feedback/Feedback.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import Container from '../../../../LayOut/Container';
+import SectionTitle from '../../../Shared/SectionTitle/SectionTitle';
 
 const Feedback = () => {
+    const [feedbacks, setFeedbacks] = useState([]);
+    const [selectedReview, setSelectedReview] = useState('');
+
+    useEffect(() => {
+        fetch('Feedback.json')
+            .then(res => res.json())
+            .then(data => setFeedbacks(data));
+    }, []);
+
+    const openModal = (review) => {
+        setSelectedReview(review);
+        const modal = document.getElementById('my_modal_3');
+        modal.showModal();
+    };
+
     return (
-        <div>
-            Feedback
-        </div>
+        <Container>
+            <div>
+                <SectionTitle
+                    subText={'Testimonials'}
+                    text={'What Our clients Say About Us'}
+                ></SectionTitle>
+                <p className='text-right mb-2'>Total Feedback : {feedbacks.length}</p>
+
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={10}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                    }}
+                    modules={[Pagination]}
+                    className='mySwiper'>
+
+                    {feedbacks.map((feedback, index) => (
+                        <SwiperSlide className='p-4' key={index}>
+                            <div className='card  border border-black feedbackCard mb-16'>
+                                <div className='card-body m-0 p-0 '>
+                                    <div className='flex justify-center mt-5'>
+                                        <img className='h-20 w-20 rounded-full' src={feedback.picture} alt='' />
+                                    </div>
+                                    <div className='px-3'>
+                                        <h2 className='font-bold'>{feedback.name}</h2>
+                                        <h2 className='font-light'>{feedback.destination}</h2>
+                                        <p>{feedback.review.split(' ').slice(0, 15).join(' ')}</p>
+                                        {feedback.review.split(' ').length > 15 && (
+                                            <button className='p-0 text-blue-900 btn btn-link ' onClick={() => openModal(feedback.review)}>
+                                                Read more
+                                            </button>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+
+            {/* Modal */}
+            <dialog id="my_modal_3" className="modal">
+                <form method="dialog" className="modal-box">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => setSelectedReview('')}>✕</button>
+                    <h3 className="font-bold text-lg">Testimonials</h3>
+                    <p className="py-4">{selectedReview}</p>
+                </form>
+            </dialog>
+        </Container>
     );
 };
 
---
+++ src/Components/Shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SectionTitle = ({text, subText}) => {
+    return (
+        <div className='mt-10 mb-10 text-left'>
+            <p className='font-extralight'>{subText}</p> 
+            <hr className='w-1/2'/>
+            <h2 className='text-3xl font-semibold'>{text}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
