3286054300e7e385536f744ddd7b8e2357b72ca2
Habibullah Raju <raju.ra423@gmail.com>
2023-08-10 12:30:29 +0600

    created responsive top destinatin swiper

--- src/Components/Pages/Home/TopThingsToDo/TopThingsToDo.jsx
+++ src/Components/Pages/Home/TopThingsToDo/TopThingsToDo.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
 import Container from "../../../../LayOut/Container";
 
 const TopThingsToDo = () => {
@@ -48,12 +47,18 @@ const TopThingsToDo = () => {
           
             
             {
-             TopThingsTodo.map((todo,index)=>(<SwiperSlide>
+             TopThingsTodo.map((todo,index)=>(
+             
+             
+              <SwiperSlide key={index}>
                 <div>
                 <img className='h-64 w-full  relative object-cover' src={todo.imageURL} alt="" />
                 <h2 className='absolute bottom-0 pb-3 pl-2  font-extrabold text-3xl text-white left-0 shadow-2xl  bg-gradient-to-t from-black'>{todo.item_name}</h2>
                 </div>
-              </SwiperSlide>))   
+              </SwiperSlide>
+             
+             
+              ))   
             }
          
         </Swiper>
