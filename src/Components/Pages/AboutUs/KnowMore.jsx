3504e097bff29053908a77265b33ab2892feabcf
riyad3399 <programminghero.riyad@gmail.com>
2023-08-13 00:38:09 +0600

    add new text

--- src/Components/Pages/AboutUs/KnowMore.jsx
+++ src/Components/Pages/AboutUs/KnowMore.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "../../../LayOut/Container";
+import Typical from "react-typical";
 
 const KnowMore = () => {
   return (
@@ -8,13 +9,25 @@ const KnowMore = () => {
         <div className="grid md:grid-cols-2 gap-4 justify-items-center items-center ">
           <div>
             <h3 className="text-3xl font-semibold mb-4">
-              About Our Travel Company{" "}
+              <Typical
+                loop={Infinity}
+                wrapper="w"
+                steps={[
+                  "Popular Destinations",
+                  1000,
+                  "What Our Travelers Say",
+                  1000,
+                  "Get in Touch",
+                  1000,
+                ]}
+              />
             </h3>
             <p className="text-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sed
               provident odit optio itaque, veniam at iusto quam asperiores et
               reprehenderit voluptatem facere
             </p>
+            {/* todo: aro kichu info baki ache */}
           </div>
           <div className="grid md:grid-cols-2 sm:grid-cols-2 gap-5 ">
             <img
