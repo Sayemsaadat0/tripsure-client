01440d09dc67f9a1b211eabf25022b1d938bf21d
irfanurislam <mdirfanurislam@gmail.com>
2023-08-16 17:14:02 +0600

    manage booked

--- src/Components/Pages/DashBoard/Admin/ManageBookings.jsx
+++ src/Components/Pages/DashBoard/Admin/ManageBookings.jsx
@@ -1,45 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 const ManageBookings = () => {
+  const [showCalendar, setShowCalendar] = useState(false); // State to control calendar visibility
+  const [paymentSuccessful, setPaymentSuccessful] = useState(false); // State to track payment status
+
+  const handleCalendarClick = () => {
+    setShowCalendar(!showCalendar);
+  };
+
+  // Simulate payment success/failure
+  const handlePayment = () => {
+    // In a real application, you would handle payment processing and set the payment status accordingly.
+    setPaymentSuccessful(true);
+  };
+
     return (
-        <div>
-              <div className='w-full'>
+        <div className='w-full '>
+              <div className=''>
             <h2 className='text-center mb-10'> Manage Bookings </h2>
             <div className="overflow-x-auto">
   <table className="table w-full">
-    {/* head */}
+   
     <thead>
-          <tr>
+          <tr className='text-lg text-black font-bold'>
                         <th>#</th>
+                        <th>Image</th>
                         <th>Name</th>
-                        <th>Email</th>
-                        <th>Manage Books</th>
-                        <th>Action</th>
+                        <th>AGency</th>
+                        <th>Calender</th>
+                        <th>Payment</th>
                     </tr>
                 </thead>
     <tbody>
-      {/* row 1 */}
+     
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+         1
         </th>
         <td>
           <div className="flex items-center space-x-3">
             <div className="avatar">
               <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="tour image" />
+                <img src="https://i.ibb.co/Hr6khp9/author3.jpg" alt="tour image" />
               </div>
             </div>
           </div>
         </td>
+        <td>travel Omuk</td>
         <td>Mdshihab @gmail.com</td>
-        <td>handle</td>
-        <th>
-          <button className="btn btn-outline bg-pink-700  text-white">delete</button>
-        </th>
-      </tr>
+        <td onClick={handleCalendarClick} style={{ cursor: 'pointer' }}>
+                  {showCalendar ? (
+                    <Calendar /> // Display the calendar component when clicked
+                  ) : (
+                    'Date Books'
+                  )}
+                </td>
+        <td className='text-xl'>{paymentSuccessful ? '✔️' : '❌'}</td>
+       
+              </tr>
       
      
       
@@ -48,6 +69,10 @@ const ManageBookings = () => {
     
   </table>
 </div>
+<div className='hidden'>
+<button className='btn' onClick={handlePayment}>Make Payment</button>
+</div>
+
         </div>
         </div>
     );
--- src/Components/Pages/DashBoard/Admin/ManagePackage.jsx
+++ src/Components/Pages/DashBoard/Admin/ManagePackage.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ManagePackage = () => {
+ 
     return (
-        <div>
-            this is the manage package
+        <div className='w-full '>
+        <div className=''>
+      <h2 className='text-center mb-10'> Manage Bookings </h2>
+      <div className="overflow-x-auto">
+<table className="table w-full">
+
+<thead>
+    <tr className='text-lg text-black font-bold'>
+                  <th>#</th>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>AGency</th>
+                  <th>Calender</th>
+                  <th>Payment</th>
+              </tr>
+          </thead>
+<tbody>
+
+<tr>
+  <th>
+   1
+  </th>
+  <td>
+    <div className="flex items-center space-x-3">
+      <div className="avatar">
+        <div className="mask mask-squircle w-12 h-12">
+          <img src="https://i.ibb.co/Hr6khp9/author3.jpg" alt="tour image" />
         </div>
+      </div>
+    </div>
+  </td>
+  <td>travel Omuk</td>
+  <td>Mdshihab @gmail.com</td>
+  <td> details
+          </td>
+  <td className='text-xl'>details</td>
+ 
+        </tr>
+
+
+
+</tbody>
+
+
+</table>
+</div>
+
+  </div>
+  </div>
     );
 };
 
