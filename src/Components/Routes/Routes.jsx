57ddfcc960cf316e37ae9d7fa7448e793c2f2c8e
Habibullah Raju <raju.ra423@gmail.com>
2023-08-17 23:23:37 +0600

    pull to test

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -73,7 +73,50 @@ const router = createBrowserRouter([
   {
     path: '/travelerChoose-thingsToDo/:category',
     element:<TravelerChoose></TravelerChoose>
-  }
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard></Dashboard>,
+    children: [
+        {
+          path: 'adminhome',
+          element: <AdminHome></AdminHome>
+        },
+      
+        {
+          path: 'admin/totalview',
+          element: <Totallview />
+        },
+        {
+          path: 'manageusers',
+          element: <ManageUser></ManageUser>
+        },
+        {
+          path: 'managebookings',
+          element: <ManageBookings />
+        },
+        {
+          path: 'managepackage',
+          element: <ManagePackage />
+        },
+        {
+          path: 'reviews',
+          element: <Reviews />
+        },
+        {
+          path: 'inquires',
+          element: <Inquiries />
+        },
+        {
+          path: 'addtour',
+          element: <AddTour />
+        },
+  ]
+  },
+
+
+ 
+ 
 
 ]);
 
