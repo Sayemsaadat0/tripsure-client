4541a0f2fc7b9197775dc6fdb4b4889f2f5c75c7
irfanurislam <mdirfanurislam@gmail.com>
2023-08-21 22:47:10 +0600

    add resturants

--- src/api/bookshotels.js
+++ src/api/bookshotels.js
@@ -10,3 +10,18 @@ export const addRoom = async roomData => {
     const data = await response.json()
     return data
   }
+
+
+
+export const addresturants = async resturantsData => {
+    const response = await fetch(`http://localhost:1000/dashboard/addresturants`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(resturantsData),
+    })
+  
+    const data = await response.json()
+    return data
+  }
