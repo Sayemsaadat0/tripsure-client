260dd1b2d3ca0b028717f8ca0fd42f8fe30da757
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-08-23 22:34:28 +0600

    sayem

--- src/Components/Pages/Home/TravelDeals/TravelDeals.jsx
+++ src/Components/Pages/Home/TravelDeals/TravelDeals.jsx
@@ -1,37 +1,70 @@
-import React from 'react';
-import SectionTitle from '../../../Shared/SectionTitle/SectionTitle';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 import Container from '../../../../LayOut/Container';
-
+import SectionTitle from '../../../Shared/SectionTitle/SectionTitle';
+import { Link } from 'react-router-dom';
 
 const TravelDeals = () => {
+  const [travelDeals, setTravelDeals] = useState([]);
 
-    return (
-        <div>
-            <SectionTitle subText={'Adventures Await'}
-                text={'Hot Travel '} coloredText={'Deals'}>
-            </SectionTitle>
+  useEffect(() => {
+    fetch('./TravelDeals.json')
+      .then(res => res.json())
+      .then(data => setTravelDeals(data));
+  }, []);
 
-            <Container>
-
-                <div>
-                    <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 md:p-4'>
-                        {/* card */}
-                        <div className="card card-compact  bg-white shadow-xl relative">
-                            <p className='badge  absolute right-1 top-3 animate-bounce'>30%</p>
-                            <figure><img src="https://i.pinimg.com/564x/0b/d3/c6/0bd3c636bd80be4e3bb0232d8ec83985.jpg" alt="title" /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">title!</h2>
-                                <p>Address</p>
-                                <div className="">
-                                    <button className="btn  ">Buy Now</button>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div data-AOS="fade-left" data-aos-duration="3000" data-aos-easing="ease">
+      <Container>
+        <div >
+        <SectionTitle subText={'Create Memories'} text={'Select Your Ideal'} coloredText={'Tour Package'} />
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+            }}
+            modules={[Pagination]}
+            className='mySwiper'
+          >
+            {travelDeals.map((deals, index) => (
+              <SwiperSlide className='lg:p-10' key={index}>
+                <div className="card card-compact bg-white shadow-2xl relative ">
+                  <p className='badge badge-primary absolute right-1 top-3 animate-bounce p-2'>TravelDeals</p>
+                  <figure><img className='rounded-lg' src={deals.picture} alt={deals.title} /></figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{deals.title}</h2>
+                    <p>{deals.destination}</p>
+                    <div>
+                      {/* todo habibullah- bhai id diye data aina diyen */}
+                      <Link to={`/TravelDeals/${deals?._id}`} className="btn">View Details</Link>
                     </div>
+                  </div>
                 </div>
-            </Container>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default TravelDeals;
