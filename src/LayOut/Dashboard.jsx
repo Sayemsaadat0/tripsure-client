c9db7bee2bd7197c555299265c208abaa012a27e
irfanurislam <mdirfanurislam@gmail.com>
2023-08-24 16:03:33 +0600

    manager users deleted button

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -3,22 +3,22 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 
 const Dashboard = () => {
-  const {user} = useAuth()
-  console.log(user)
-  const [usersdata ,setUsersData] = useState([])
-  useEffect(() =>{
-    fetch(`http://localhost:1000/users`)
-    .then(res => res.json())
-    .then(data =>{
-      setUsersData(data)
-    })
-  },[])
-  console.log('shihab',usersdata)
-  
-  const activeUserEmail = user?.email
-  console.log(activeUserEmail)
-  const findUser = usersdata?.find(user => user?.email === activeUserEmail)
-  console.log('role',findUser)
+  // const { user } = useAuth();
+  // console.log(user);
+  // const [usersdata, setUsersData] = useState([]);
+  // useEffect(() => {
+  //   fetch(`http://localhost:1000/users`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setUsersData(data);
+  //     });
+  // }, []);
+  // console.log("shihab", usersdata);
+
+  // const activeUserEmail = user?.email;
+  // console.log(activeUserEmail);
+  // const findUser = usersdata?.find((user) => user?.email === activeUserEmail);
+  // console.log("role", findUser);
 
   return (
     <div className="drawer lg:drawer-open ">
@@ -38,51 +38,51 @@ const Dashboard = () => {
         <ul className="menu p-4 w-80 h-full bg-[#7bb1eb] bg-opacity-40  text-xl">
           {/* Sidebar content here */}
           <div className="navbar mb-10">
-          <NavLink to='/'>
-             
-                   <img className='md:w-full md:h-[30%] w-[120px] max-w-[180px]' src="https://i.ibb.co/F6hSztb/2.png" alt="" /> 
-        </NavLink>
-
+            <NavLink to="/">
+              <img
+                className="md:w-full md:h-[30%] w-[120px] max-w-[180px]"
+                src="https://i.ibb.co/F6hSztb/2.png"
+                alt=""
+              />
+            </NavLink>
           </div>
 
-         
-
-          {findUser?.role === 'admin' &&(
+          {/* {findUser?.role === "admin" && (
             <>
-            <li>
-            <NavLink to="/dashboard/adminhome">Home</NavLink>
-          </li>
-          <li className="mt-2">
-            <NavLink to="/dashboard/manageusers">Manage users</NavLink>
-          </li>
-          <li  className="mt-2">
-            <NavLink to="/dashboard/managebookings">Manage Bookings</NavLink>
-          </li>
-          <li  className="mt-2">
-            <NavLink to="/dashboard/managepackage">Manage Package</NavLink>
-          </li>
-          <li  className="mt-2">
-            <NavLink to="/dashboard/reviews">Reviews</NavLink>
-          </li>
-         
-          <li className="mt-2">
-            <NavLink to="/dashboard/addresource">Add resource</NavLink>
-          </li>
-          
-            </>
-          )}
+              <li>
+                <NavLink to="/dashboard/adminhome">Home</NavLink>
+              </li>
+              <li className="mt-2">
+                <NavLink to="/dashboard/manageusers">Manage users</NavLink>
+              </li>
+              <li className="mt-2">
+                <NavLink to="/dashboard/managebookings">
+                  Manage Bookings
+                </NavLink>
+              </li>
+              <li className="mt-2">
+                <NavLink to="/dashboard/managepackage">Manage Package</NavLink>
+              </li>
+              <li className="mt-2">
+                <NavLink to="/dashboard/reviews">Reviews</NavLink>
+              </li>
 
-          {findUser?.role === 'operator' &&(
+              <li className="mt-2">
+                <NavLink to="/dashboard/addresource">Add resource</NavLink>
+              </li>
+            </>
+          )} */}
+
+          {/* {findUser?.role === "operator" && (
             <>
-             <li className="mt-2">
-            <NavLink to="/dashboard/inquires">inquires</NavLink>
-          </li>
-
+              <li className="mt-2">
+                <NavLink to="/dashboard/inquires">inquires</NavLink>
+              </li>
             </>
-          )}
+          )} */}
 
           {/* users Home */}
-
+          {/* 
         
          {!findUser?.role && (
     <>
@@ -96,8 +96,35 @@ const Dashboard = () => {
           <NavLink to="/dashboard/addtour">Add tour</NavLink>
         </li>
     </>
-  )}
-  
+  )} */}
+
+          <li>
+            <NavLink to="/dashboard/adminhome">Home</NavLink>
+          </li>
+          <li className="mt-2">
+            <NavLink to="/dashboard/manageusers">Manage users</NavLink>
+          </li>
+          <li className="mt-2">
+            <NavLink to="/dashboard/managebookings">Manage Bookings</NavLink>
+          </li>
+          <li className="mt-2">
+            <NavLink to="/dashboard/managepackage">Manage Package</NavLink>
+          </li>
+          <li className="mt-2">
+            <NavLink to="/dashboard/reviews">Reviews</NavLink>
+          </li>
+
+          <li className="mt-2">
+            <NavLink to="/dashboard/addresource">Add resource</NavLink>
+          </li>
+          <li className="mt-2">
+                <NavLink to="/dashboard/inquires">inquires</NavLink>
+             </li>
+
+         
+          <li className="mt-2">
+            <NavLink to="/dashboard/addtour">Add tour</NavLink>
+          </li>
         </ul>
       </div>
     </div>
