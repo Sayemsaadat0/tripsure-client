5d0acdc6f6a610e52e2416befe18a397bb5ff67d
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-09-12 02:59:24 +0600

    sayem open ai

--- src/Components/Pages/Home/OpenAi/OpenAi.jsx
+++ src/Components/Pages/Home/OpenAi/OpenAi.jsx
@@ -13,33 +13,33 @@ const OpenAi = () => {
     const words = ['Can you suggest a travel destination for a romantic getaway in November?',]
 
     return (
-        <Container>
 
-            <div className='md:flex items-center justify-center my-20 mx-auto  md:justify-between w-full bg-blue-50 p-10' >
+        <div className='bg-blue-50'>
+                <div className='md:flex max-w-[1920px] items-center justify-center my-20 mx-auto  md:justify-between w-full  p-10' >
 
-                {/* forom */}
-                <div className='md:w-1/2 '>
+                    {/* forom */}
+                    <div className='md:w-1/2 '>
 
-                    <form className='flex flex-col  py-20 justify-center items-center ' onSubmit={handleSubmit(onSubmit)}>
-                        <h3>having trouble to make desicion? ask anything !</h3>
+                        <form className='flex flex-col  py-20 justify-center items-center ' onSubmit={handleSubmit(onSubmit)}>
+                            <h3>having trouble to make desicion? ask anything !</h3>
 
-                        <input
-                            className='input input-bordered input-md lg:w-[55%] w-72   my-5 '
-                            type="text"
-                            placeholder="questions"
-                            {...register("question", { required: true, maxLength: 30 })} />
+                            <input
+                                className='input input-bordered input-md lg:w-[55%] w-72   my-5 '
+                                type="text"
+                                placeholder="questions"
+                                {...register("question", { required: true, maxLength: 30 })} />
 
-                        <button className='btn-primary lg:w-[55%] w-72' type='submit'>Send</button>
+                            <button className='btn-primary lg:w-[55%] w-72' type='submit'>Send</button>
 
-                        <p className='text-center  py-10 '>answerd messege answerd messegeanswerd messege  answerd messege sdsdjs sdhsd  sjdsd  sjdskdj  sjds dsjdskd  askdjsd  asdjd answerd messege</p>
-                    </form>
+                            <p className='text-center  py-10 '>answerd messege answerd messegeanswerd messege  answerd messege sdsdjs sdhsd  sjdsd  sjdskdj  sjds dsjdskd  askdjsd  asdjd answerd messege</p>
+                        </form>
+                    </div>
+                    {/* ........ */}
+                    <div className='md:w-1/2 flex justify-center'>
+                        <img className='md:w-[55%] ' src="https://i.ibb.co/TRNM1Mq/Untitled-design-6.png" alt="" />
+                    </div>
                 </div>
-                {/* ........ */}
-                <div className='md:w-1/2 flex justify-center'>
-                    <img className='md:w-[55%] ' src="https://i.ibb.co/TRNM1Mq/Untitled-design-6.png" alt="" />
-                </div>
-            </div>
-        </Container>
+        </div>
     );
 };
 
